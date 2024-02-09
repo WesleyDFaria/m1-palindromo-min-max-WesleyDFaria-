53c1e2b59165a081674e1defddceca47ad9f7a74
Wesley Faria <wfaria49@gmail.com>
2024-02-08 21:45:01 -0300

    Entrega Palíndromo

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(palavraFrase) {
+  let text = palavraFrase.replace(/\s+/g, "").toLowerCase();
+  let result = text.length;
+
+  for (let i = 0; i < result; i++) {
+    if (text[i] !== text[result - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+  let max = arr[0];
+  let min = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+  }
+  return [min, max];
 }
